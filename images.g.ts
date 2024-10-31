f792fa9bc2c704e84af2937710c2112c48400c1c
agneslam777 <agneszhenqiilam0827@gmail.com>
2024-10-31 17:36:25 +1100

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,63 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "projectile":return img`
+. . f f f . . . . . . . . . . . 
+f f f e e f f f . . . . f f f f 
+f f e e e f e e f . f f e e e e 
+f f e 3 e e 3 e e f f e e e e f 
+f f e 3 e e 3 e e f e e e e f f 
+f e e e e e e e e f e e e e f . 
+e e 1 e e e 1 e e e e e e e f . 
+e e e e e e e e e e e e e e f . 
+e e 1 f f 1 e e e e e e e e f . 
+e f 1 f f 1 f e e e e f e e f . 
+f f f f f f f e e e e f e e f . 
+f f 2 2 2 2 f e e e e f e f . . 
+. f 2 2 2 2 2 e e e e f f . . . 
+. . f 2 2 2 e e e e f . . . . . 
+. . . f f f f f f f . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image2":
+            case "treasure":return img`
+. b b b b b b b b b b b b b b . 
+b e 4 4 4 4 4 4 4 4 4 4 4 4 4 b 
+b e 4 4 4 4 4 4 4 4 4 4 4 4 e b 
+b e e 4 4 4 4 4 4 4 4 4 4 e e b 
+b b b b b b b d d b b b b b b b 
+. b b b b b b c c b b b b b b . 
+b 5 5 5 5 5 b c c b 5 5 5 5 5 b 
+b 5 5 5 5 5 5 b b 5 5 5 5 5 5 b 
+b 5 5 5 5 5 5 5 5 5 5 5 5 5 5 b 
+b 5 5 5 5 5 5 5 5 5 5 5 5 5 5 b 
+b b b b 5 b b b b b b 5 b b b b 
+b e e e 5 e e e e e e 5 e e e b 
+b e e e 5 e e e e e e 5 e e e b 
+b c e e 5 e e e e e e e e e c b 
+b b b b 5 b b b b b b b b b b b 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
